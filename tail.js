3e6ffe0d977aefb55cdb551ab9c85a282347f87d
ZSully09 <zsullivan93@gmail.com>
2019-10-16 20:07:46 +0000

    lotide commit for D1 tasks

---
+++ tail.js
@@ -0,0 +1,15 @@
+var assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+function tail(array) {
+  let newArray = [];
+  for (let i = 1; i < array.length; i++) {
+    newArray.push(array[i]);
+  }
+  return newArray;
+}
+assertEqual(tail([5, 6, 7]), 6);
