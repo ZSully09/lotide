67de06b8cd941669296f701f2d1dd8e756c52acf
ZSully09 <zsullivan93@gmail.com>
2019-10-16 20:09:22 +0000

    Initial test, failing as assertEqual only compares primitive values

---
+++ countOnly.js
@@ -0,0 +1,36 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// allItems: an array of string that we need to look through
+// itemsToCount: an object specifiying what to count
+
+const countOnly = function(allItems, itemsToCount) {};
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, {
+  Jason: true,
+  Karima: true,
+  Fang: true
+});
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+
+// https://web.compass.lighthouselabs.ca/days/w01d3/activities/821
