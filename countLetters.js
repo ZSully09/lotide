1f96e5f984f877f58465c33b819ede714be7664c
ZSully09 <zsullivan93@gmail.com>
2019-10-16 23:59:11 +0000

    counting letters in 'lighthouse in the house'

--- countLetters.js
+++ countLetters.js
@@ -5,3 +5,21 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
+const countLetters = function(str) {
+  const result = {};
+  const newString = str.split(" ").join("");
+  // console.log(newString); test 1
+  for (const letter of newString) {
+    if (result[letter]) {
+      //add one
+      result[letter] += 1;
+    } else {
+      // set letter equal to one
+      result[letter] = 1;
+    }
+  }
+  return result;
+};
+
+console.log(countLetters("lighthouse in the house"));
