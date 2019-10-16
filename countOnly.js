f55af76bac8465e724983523bf8403f1c05d49f2
ZSully09 <zsullivan93@gmail.com>
2019-10-16 23:57:41 +0000

    tinkering

--- countOnly.js
+++ countOnly.js
@@ -42,7 +42,6 @@ const result1 = countOnly(firstNames, {
   Fang: true
 });
 // console.log(result1);
-
 assertEqual(result1["Jason"], 1);
 assertEqual(result1["Karima"], undefined);
 assertEqual(result1["Fang"], 2);
