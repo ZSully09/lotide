536946db49205d77839bb27c5f16236bbfaa1df0
ZSully09 <zsullivan93@gmail.com>
2019-10-18 03:16:13 +0000

    update eqArrays such that the if statement confirms array 1 length and array 2 length are the same, and if not, it will return false

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,7 +1,11 @@
 const eqArrays = function(array1, array2) {
-  for (let i = 0; i < 3; i++) {
-    if (array1[i] === array2[i]) {
-    } else {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    // test 1: console.log("start of loop", array1[i]);
+    if (array1[i] !== array2[i]) {
+      //test 2: console.log("non match found", array1[i], array2[i]);
       return false;
     }
   }
