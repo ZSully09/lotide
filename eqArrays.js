12fbcac6daa44408253f2b988aef7672fe6414ce
ZSully09 <zsullivan93@gmail.com>
2019-10-19 23:23:11 +0000

    correcting tests

--- eqArrays.js
+++ eqArrays.js
@@ -20,20 +20,20 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
-eqArrays([1, 2, 3], [3, 2, 1]); // => false
-
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
-
-eqArrays(["1", "2", "3", "4"], ["1", "2", "3", "4"]); // => true
-eqArrays(["1", "2", "3", "4"], ["1", "2", "3", "5"]); // => false
-
-eqArrays(["1", "2", "3"], ["1"]); // => false
-eqArrays(["1"], ["1", "2", "3"]); // => false
-
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // pass
-assertEqual(eqArrays([1, 2, 3, 4], [1, 2, 3]), false); //fail
+assertEqual(eqArrays([1, 2, 3, 4], [1, 2, 3]), false); //pass
 assertEqual(eqArrays([1, 2, 3], [1, 2, 5]), true); // fail
 assertEqual(eqArrays([1, 2, 3], [1, 2, 5]), false); // pass
+
+// eqArrays([1, 2, 3], [1, 2, 3]); // => true
+// eqArrays([1, 2, 3], [3, 2, 1]); // => false
+
+// eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+// eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+// eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+
+// eqArrays(["1", "2", "3", "4"], ["1", "2", "3", "4"]); // => true
+// eqArrays(["1", "2", "3", "4"], ["1", "2", "3", "5"]); // => false
+
+// eqArrays(["1", "2", "3"], ["1"]); // => false
+// eqArrays(["1"], ["1", "2", "3"]); // => false
