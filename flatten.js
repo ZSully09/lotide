dc40a41e66c2ae155bd0e201421e95fa39d1a17b
ZSully09 <zsullivan93@gmail.com>
2019-10-20 02:36:57 +0000

    added additional tests

--- flatten.js
+++ flatten.js
@@ -42,3 +42,7 @@ const flatten = function(array) {
 };
 
 assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
+
+assertArraysEqual(flatten([[1, 2, 3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
+
+assertArraysEqual(flatten([1, [2, 3, 4], [5, 6]]), [1, 2, 3, 4, 5, 6]);
