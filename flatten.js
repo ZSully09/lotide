42a5e691d2fefdea32067110f8ec8f0f7df8f2bb
ZSully09 <zsullivan93@gmail.com>
2019-10-20 02:33:31 +0000

    implmented the flatten function and inserted console tests

--- flatten.js
+++ flatten.js
@@ -26,7 +26,19 @@ const assertArraysEqual = function(array1, array2) {
 const flatten = function(array) {
   // Create a new array
   var newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      for (let x = 0; x < array[i].length; x++) {
+        newArray.push(array[i][x]);
+        // console.log(newArray);
+      }
+      // console.log(newArray);
+    } else {
+      newArray.push(array[i]);
+    }
+    // console.log(newArray);
+  }
+  return newArray;
 };
 
-assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]));
-// => [1, 2, 3, 4, 5, 6]
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
