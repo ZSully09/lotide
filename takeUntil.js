26027fb34d4ca01085a8afa875b27c42f139c54b
ZSully09 <zsullivan93@gmail.com>
2019-10-20 03:59:32 +0000

    inputted instructions

---
+++ takeUntil.js
@@ -0,0 +1,7 @@
+// Create the function takeUntil which will take in two parameters:
+//    1. The array to work with
+//    2. The callback (which Lodash calls "predicate")
+
+const takeUntil = function(array, callback) {
+  // ..
+};
