9f5380b157cb4ef8fa00d77b5681e1e9ff79b1e9
ZSully09 <zsullivan93@gmail.com>
2019-10-20 05:26:55 +0000

    added commentary

--- takeUntil.js
+++ takeUntil.js
@@ -26,11 +26,13 @@ const assertArraysEqual = function(array1, array2) {
 //    2. The callback (which Lodash calls "predicate")
 
 const takeUntil = function(array, callback) {
+  // iterate over the array
   for (let i = 0; i < array.length; i++) {
+    // when callback returns true (Results1: x < 0; Results2 x === ",") at element i return a slice of the array from index 0 up to but not including the element (i) where the condition is met
     if (callback(array[i])) {
       return array.slice(0, i);
     }
-  }
+  } //if condition is never met return the original array
   return array;
 };
 
