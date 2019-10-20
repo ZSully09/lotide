2813880922a32cbd14a766bd9a0df280e37a100f
ZSully09 <zsullivan93@gmail.com>
2019-10-20 03:46:22 +0000

    implement findKeyByValue function with for in loop

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,3 +1,5 @@
+// Implement the function findKeyByValue which takes in an object and a value. It should scan the object and return the first key which contains the given value. If no key with that given value is found, then it should return undefined.
+
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
@@ -5,3 +7,25 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
+// Implement findKeyBy Value function
+const findKeyByValue = function(obj, val) {
+  // Implement for of loop
+  for (const key in obj) {
+    // console.log(key);
+    // console.log(obj[key]);
+    if (obj[key] === val) {
+      // console.log(val);
+      return key;
+    }
+  }
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
