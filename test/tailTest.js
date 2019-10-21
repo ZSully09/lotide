888d03e4e263fd98f147e86ae4cf9f858cc77b18
ZSully09 <zsullivan93@gmail.com>
2019-10-21 20:46:19 +0000

    implemented mocha and chai unit tests for tailTest and middleTest

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,17 @@
-const assertEqual = require("../assertEqual");
+const assert = require("chai").assert;
 const tail = require("../tail");
 
-const result = tail(["Lighthouse", "Labs", "Week2"]);
-
-assertEqual(result[1], "Week2"); // Pass
-assertEqual(result[0], "Labs"); // Pass
+describe("# tail", () => {
+  // 1
+  it("returns [Labs, Week2] for [Lighthouse, Labs, Week2]", () => {
+    assert.deepEqual(tail(["Lighthouse", "Labs", "Week2"]), ["Labs", "Week2"]);
+  });
+  // 2
+  it("returns empty from empty", () => {
+    assert.deepEqual(tail([]), []);
+  });
+  // 3
+  it("returns empty from [1]", () => {
+    assert.deepEqual(tail([1]), []);
+  });
+});
