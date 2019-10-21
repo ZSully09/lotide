c289ef3525ffde27ee20d88e7a55f382b71cc330
ZSully09 <zsullivan93@gmail.com>
2019-10-21 19:31:26 +0000

    eqArrays refactoring and eqArraysTest exporting

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
@@ -19,21 +11,4 @@ const eqArrays = function(array1, array2) {
   }
   return true;
 };
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // pass
-assertEqual(eqArrays([1, 2, 3, 4], [1, 2, 3]), false); //pass
-assertEqual(eqArrays([1, 2, 3], [1, 2, 5]), true); // fail
-assertEqual(eqArrays([1, 2, 3], [1, 2, 5]), false); // pass
-
-// eqArrays([1, 2, 3], [1, 2, 3]); // => true
-// eqArrays([1, 2, 3], [3, 2, 1]); // => false
-
-// eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-// eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-// eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
-
-// eqArrays(["1", "2", "3", "4"], ["1", "2", "3", "4"]); // => true
-// eqArrays(["1", "2", "3", "4"], ["1", "2", "3", "5"]); // => false
-
-// eqArrays(["1", "2", "3"], ["1"]); // => false
-// eqArrays(["1"], ["1", "2", "3"]); // => false
+module.exports = eqArrays;
