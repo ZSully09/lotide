63fab8b6cd378a0b2e894d3c9342681773c2e9d3
ZSully09 <zsullivan93@gmail.com>
2019-10-21 06:06:59 +0000

    added beginning of findKey function and a test

--- findKey.js
+++ findKey.js
@@ -10,14 +10,16 @@ const assertEqual = function(actual, expected) {
 const findKey = function(object, callback) {};
 
 //Implement test
-findKey(
-  {
-    "Blue Hill": { stars: 1 },
-    Akaleri: { stars: 3 },
-    noma: { stars: 2 },
-    elBulli: { stars: 3 },
-    Ora: { stars: 2 },
-    Akelarre: { stars: 3 }
-  },
-  x => x.stars === 2
+assertEqual(
+  findKey(
+    {
+      "Blue Hill": { stars: 1 },
+      Akaleri: { stars: 3 },
+      noma: { stars: 2 },
+      elBulli: { stars: 3 },
+      Ora: { stars: 2 },
+      Akelarre: { stars: 3 }
+    },
+    x => x.stars === 2
+  )
 ); // => "noma"
