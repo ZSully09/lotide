aa48c359016d82c200078bdec20fc69a2e7c7320
ZSully09 <zsullivan93@gmail.com>
2019-10-21 22:07:34 +0000

    implement mocha and chai until tests

--- test/headTest.js
+++ test/headTest.js
@@ -2,17 +2,20 @@ const assert = require("chai").assert;
 const head = require("../head");
 
 describe("#head", () => {
+  // 1
   it("returns 1 for [1, 2, 3]", () => {
     assert.strictEqual(head([1, 2, 3]), 1);
   });
+  // 2
   it("returns '5' for ['5']", () => {
     assert.strictEqual(head(["5"]), "5");
   });
+  // 3
   it("returns '5' for ['5, 6, 7']", () => {
     assert.strictEqual(head([5, 6, 7]), 5);
   });
+  // 4
   it("returns '1' for ['1, 6, 12']", () => {
     assert.strictEqual(head([1, 6, 12]), 1);
   });
-  // asdf
 });
