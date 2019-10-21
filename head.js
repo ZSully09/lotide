f50bad1a151875b1220904bb6a281ffb7f6b2d42
ZSully09 <zsullivan93@gmail.com>
2019-10-21 18:01:59 +0000

    added additional tests

--- head.js
+++ head.js
@@ -8,4 +8,7 @@ const assertEqual = function(actual, expected) {
 function head(array) {
   return array[0];
 }
-assertEqual(head([5, 6, 7]), 5);
+assertEqual(head([5, 6, 7]), 5); // Pass
+assertEqual(head([1, 6, 12]), 1); // Pass
+assertEqual(head([5, 6, 7]), 7); // Fail
+assertEqual(head([1, 6, 12]), ""); // Fail
