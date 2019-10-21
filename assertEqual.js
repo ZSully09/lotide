2062b08e1be2f87bc67d4c1d1dfc8d2b859e8ba8
ZSully09 <zsullivan93@gmail.com>
2019-10-21 19:35:31 +0000

    assert Equal refactoring and exporting module to test file

--- assertEqual.js
+++ assertEqual.js
@@ -6,8 +6,3 @@ const assertEqual = function(actual, expected) {
   }
 };
 module.exports = assertEqual;
-
-assertEqual("LHL", "LHL"); // Pass
-assertEqual("LHL", "LH"); // Fail
-assertEqual("Lighthouse Labs", "Lighthouse Labs"); // Pass
-assertEqual("Lighthouse labs", "Lighthouse Labs"); // Fail
---
+++ test/assertEqualTest.js
@@ -0,0 +1,6 @@
+const assertEqual = require("../assertEqual");
+
+assertEqual("LHL", "LHL"); // Pass
+assertEqual("LHL", "LH"); // Fail
+assertEqual("Lighthouse Labs", "Lighthouse Labs"); // Pass
+assertEqual("Lighthouse labs", "Lighthouse Labs"); // Fail
