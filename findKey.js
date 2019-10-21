7b6f3538cdb31a7aaad5825bd905d4eca2d1686f
ZSully09 <zsullivan93@gmail.com>
2019-10-21 06:25:17 +0000

    added the for in loop & if statement invoking the callback

--- findKey.js
+++ findKey.js
@@ -7,7 +7,13 @@ const assertEqual = function(actual, expected) {
 };
 
 // Implement findKey function
-const findKey = function(object, callback) {};
+const findKey = function(object, callback) {
+  for (const key in object) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+};
 
 //Implement test
 assertEqual(
@@ -23,4 +29,19 @@ assertEqual(
     x => x.stars === 2
   ),
   "noma"
+);
+
+assertEqual(
+  findKey(
+    {
+      "Blue Hill": { stars: 1 },
+      Akaleri: { stars: 3 },
+      noma: { stars: 2 },
+      elBulli: { stars: 3 },
+      Ora: { stars: 2 },
+      Akelarre: { stars: 3 }
+    },
+    x => x.stars === 3
+  ),
+  "Akaleri"
 );
